57832bcd02ac0be0ec87dcf5b4353bc78ea38147
guaracyalima <guaracyaraujolima@gmail.com>
2017-11-06 12:53:37 -0200

    angular_quick_start

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+angular.module('primeiraApp').config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('dashboard', {
+      url: "/dashboard",
+      templateUrl: "dashboard/dashboard.html"
+    })
+
+    $urlRouterProvider.otherwise('/dashboard')
+  }
+])
